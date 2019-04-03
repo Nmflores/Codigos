61b4e464229a34e369ac9fc1a23d5162e35519ef
Nicolas Machado Flores <nicolas.flores.contato@gmail.com>
2019-04-03 18:49:50 -0300

    Criei um site de testes para praticar

--- js/teste.js
+++ js/teste.js
@@ -1,6 +1,10 @@
 $(document).ready(function teste(){
-        $("h1").click(function(){
-            $("p").css("color","blue");
+        $(".brincadeira").click(function(){
+            $(this).text("\u00A9Criado por mim");
+
+        })
+        $(".transforma").click(function(){
+            $("h1").text("Brincando com Jquery");
 
         })
 
