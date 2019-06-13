4acb23b9972ab5bd5b1b1661d4917696d8732a46
Nicolas <achronys@gmail.com>
2019-06-13 14:27:15 -0300

    Fiz umas mudanças no html

--- Python/Lista.py
+++ Python/Lista.py
@@ -3,8 +3,19 @@ class No:
         self.dado=valor
         self.next=None
         self.previous=None
+    def get_next(self):
+        return self.next
 
-
+    def set_next(self,novo_next):
+        self.next = novo_next
+    def get_previous(self):
+        return self.previous
+    def set_previous(self,novo_previous):
+        self.previous = novo_previous  
+    def get_dado(self):
+        return self.dado 
+    def set_dado(self,novo_dado):
+        self.dado = novo_dado     
 class Lista:
     def __init__(self):
         self.head=None
@@ -14,20 +25,45 @@ class Lista:
 
         if self.head is None:
             self.head = self.tail =No(valor)
-        else:
-            self.tail.next=No(valor)
-            self.tail=self.tail.next
+         
 
+        else:
+            self.tail.set_next(No(valor))
+            self.tail=self.tail.get_next()
+          
+
+    def remove(self):
+        i = self.head
+        anterior = None
+        while i is not None:
+            if i.get_next() is self.tail:
+                anterior = i
+
+            i = i.next
+
+        anterior.set_next(None)
+
+    def adicionaPrimeiro(self,valor):
+        novoNo = No(valor)
+        novoNo.set_next(self.head)
+        self.head =  novoNo 
+                
+       
+
+    def mostrarLista(self):
+        i = lista.head
+        while i is not None:
+            print(i.dado)
+            i = i.next
 
 lista = Lista()
 
 for i in range(0,10):
     lista.append(i)
+    
+lista.mostrarLista()
+lista.remove()
+lista.mostrarLista()
 
-  
-i = lista.head
-while i is not None:
-    print(i.dado)
-    i = i.next
 
         
--- js/teste.js
+++ js/teste.js
@@ -1,20 +1,14 @@
 $(document).ready(function teste(){
-        $(".brincadeira").click(function(){
-        let valor1= $(this).text();
-        let valor2=$(this).text()
+        $("#brincadeira").click(function(){
+        let valor1 = $(this).text();
+        let valor2 =$(this).text()
 
         if(valor1 =="\u00A9Criado por Nicolas Machado Flores"){
-            $(".brincadeira").text("\u00A9Criado por mim.")
+            $("#brincadeira").text("\u00A9Criado por mim.")
         }
        if(valor2 =="\u00A9Criado por mim."){
-           $(".brincadeira").text("\u00A9Criado por Nicolas Machado Flores");
+           $("#brincadeira").text("\u00A9Criado por Nicolas Machado Flores");
        }
         
         })
-
-        $("section p").click(function(){
-            $("h1").text("Brincando com Jquery");
-
-        })
-
 });
